970896672c72086b2b67ada7d5a8b91daba5a3d2
心衍 <vier@j7yx.com>
2026-07-16 14:01:17 +0800

    Update fastinstall.ps1

--- fastinstall.ps1
+++ fastinstall.ps1
@@ -19,12 +19,14 @@ Install-Scoop
 # 设置缓存目录（下载的安装包存这里）
 scoop config cache_path 'D:\Scoop\cache'
 
+#添加仓库
 #scoop bucket add main         https://gh-proxy.com/https://github.com/ScoopInstaller/Main
 #scoop bucket add extras       https://gh-proxy.com/https://github.com/ScoopInstaller/Extras
 #scoop bucket add nonportable  https://gh-proxy.com/https://github.com/ScoopInstaller/Nonportable
 #scoop bucket add sysinternals https://gh-proxy.com/https://github.com/niheaven/scoop-sysinternals
 #scoop bucket add svier0       https://gh-proxy.com/https://github.com/svier0/scoopbucket
 
+#浅克隆 不附带仓库历史
 git clone --depth 1 https://gh-proxy.com/https://github.com/ScoopInstaller/Main         "$env:SCOOP\buckets\main"
 git clone --depth 1 https://gh-proxy.com/https://github.com/ScoopInstaller/Extras       "$env:SCOOP\buckets\extras"
 git clone --depth 1 https://gh-proxy.com/https://github.com/ScoopInstaller/Nonportable  "$env:SCOOP\buckets\nonportable"
